6fad9d401cd639d33b2e92d7cebc6308b9b5aba7
Edwin Tjandraatmadja <edwintjandra8@gmail.com>
2023-10-26 09:43:44 +0700

    login and register tapi belom validation

--- itdiv-mini-project/Controllers/AccountController.cs
+++ itdiv-mini-project/Controllers/AccountController.cs
@@ -1,14 +1,14 @@
 ﻿using itdiv_mini_project.Models;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using System.Data;
+using System.Security.Claims;
 
 namespace itdiv_mini_project.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController : ControllerBase
+    public class AccountController : Controller
     {
         private readonly IConfiguration _configuration;
         public AccountController(IConfiguration configuration)
@@ -16,39 +16,85 @@ namespace itdiv_mini_project.Controllers
             _configuration = configuration;
         }
 
+         public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View(); 
+        }
+
+        //registration functionallity
         [HttpPost]
-        [Route("registration")]
-        public string registration(Account account) 
+        public string Registration(Account account)
         {
             SqlConnection con = new SqlConnection(_configuration.GetConnectionString("DefaultConnection").ToString());
-            SqlCommand cmd = new SqlCommand("INSERT INTO Account( Name, Email,Password) VALUES('"+account.Name+"' , '"+ account.Email +"','"+account.Password+"')", con);
+            SqlCommand cmd = new SqlCommand("INSERT INTO Account(Name, Email, Password) VALUES('" + account.Name + "', '" + account.Email + "','" + account.Password + "')", con);
             con.Open();
             int i = cmd.ExecuteNonQuery();
             con.Close();
             if (i > 0)
             {
-                return "data inserted succesfully";
-            } else
+                return "Data inserted successfully";
+            }
+            else
             {
-                return "error";
+                return "Error";
             }
         }
 
+        public string seeMyUser() {
+            var userIdClaim = User.FindFirst("UserID");
+            if (userIdClaim != null)
+            {
+                string userId = userIdClaim.Value;
+                // Use the userId as needed
+                return userId;
+            }
+            else {
+                return "ora ono";
+            }
+        }
+        //login functionality 
         [HttpPost]
-        [Route("login")]
-        public string login(Account account) {
+        public async Task<IActionResult> Login(Account account)
+        {
             SqlConnection con = new SqlConnection(_configuration.GetConnectionString("DefaultConnection").ToString());
-            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Account WHERE Email = '"+account.Email+"' And Password= '"+account.Password+"'  ",con);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Account WHERE Email = '" + account.Email + "' AND Password = '" + account.Password + "'", con);
             DataTable dt = new DataTable();
 
+            
             da.Fill(dt);
             if (dt.Rows.Count > 0)
             {
-                return "data found";
-            } else 
+                DataRow row = dt.Rows[0]; // Assuming only one record is expected
+                String UserID =row["UserID"].ToString();
+                
+                var claims = new List<Claim>
+                  {
+                    new Claim("UserID", UserID)
+                  };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+
+                return Redirect("/"); // Redirect to the desired page
+            }
+            else
             {
-                return "invalid user";
+                return View("LoginError"); // Display a login error view
             }
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
+        }
+
+
     }
 }
--- itdiv-mini-project/Program.cs
+++ itdiv-mini-project/Program.cs
@@ -1,15 +1,19 @@
 using itdiv_mini_project.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
 //inject db context
 builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration
                 .GetConnectionString("DefaultConnection")));
 
+
+// Add services to the container.
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,13 +26,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}");
